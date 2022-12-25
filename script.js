ecd1680f506099a5bf24929430a602d66d0677df
bszeidl <szeidl.balazs@hotmail.com>
2022-12-25 13:14:12 +0100

    Basic functions

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 function pageLoaded () {
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
-    context.fillRect(175,100,100,100);  
+
+    let canvasWidth = canvas.clientWidth;
+    let canvasHeight = canvas.height;
+    let positionX = 0;
+    let positionY = positionX % 2 === 0 ? canvasHeight/2  : canvasHeight/2;
+
+    context.beginPath();
+
+    context.moveTo(positionX,positionY);
+    context.lineTo(positionX,positionY);
+    context.strokeStyle ='rgba(255,0,0,.5)';
+    context.stroke();
+
+
 
 }
 
